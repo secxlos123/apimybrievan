acde9f857be8d91de327c3a70be5e4cf5a4ffdf9
yudijohn <yudi.y@smooets.com>
2017-08-14 17:00:31 +0700

    adding nik field on customer detail table

--- app/Http/Controllers/RemovableController.php
+++ app/Http/Controllers/RemovableController.php
@@ -36,11 +36,11 @@ class RemovableController extends Controller
                 $update_message[] = 'Remove gender, phone, mobile_phone fields on customer_details table!';
             }
 
-            if( ! Schema::hasColumn( 'users', 'nik' ) ) {
-                Schema::table( 'users', function ( Blueprint $table ) {
+            if( ! Schema::hasColumn( 'customer_details', 'nik' ) ) {
+                Schema::table( 'customer_details', function ( Blueprint $table ) {
                     $table->string( 'nik' )->nullable();
                 } );
-                $update_message[] = 'Add nik field on user table!';
+                $update_message[] = 'Add nik field on customer_details table!';
             }
 
             if( empty( $update_message ) ) {
