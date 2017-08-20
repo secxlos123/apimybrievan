ee617727f6cef48f06474c94501d1d62cbaca28c
yudijohn <yudi.y@smooets.com>
2017-08-20 11:00:23 +0700

    tets eform

--- app/Models/EForm.php
+++ app/Models/EForm.php
@@ -49,6 +49,7 @@ class EForm extends Model
 
         static::creating( function( $eform ) {
             $customer_detail = CustomerDetail::whereNik( $eform->nik )->first();
+            dd( $customer_detail );
             $eform->user_id = $customer_detail->user_id;
         } );
     }
