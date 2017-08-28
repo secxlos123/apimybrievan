69d0b4521d3718aac854b1c0b19aedbb437f530f
Candra Sudirman <candra.s@smooets.com>
2017-08-28 18:47:21 +0700

    update profile developer

---
+++ app/Http/Controllers/API/v1/Eks/ProfileController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Eks;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\TempUser;
+use App\Http\Requests\API\v1\Profile\UpdateRequest;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $profile = User::getProfile($request);
+        return response()->success(['data' => $profile]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRequest $request)
+    {
+        $user = $request->user();
+        
+        if ($user->inRole('developer') || $user->inRole('others')) {
+            $request->merge(['user_id' => $user->id]);
+            $temp = TempUser::updateOrCreate(['user_id' => $user->id], $request->all());
+        }
+
+        return response()->success(['message' => 'Data profile berhasil dirubah.', 'data' => (object) null]);
+    }
+}
