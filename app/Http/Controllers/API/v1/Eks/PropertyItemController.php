082de9c22d4ad67fc4e3bcf4e3458e0f10be868d
Candra Sudirman <candra.s@smooets.com>
2017-09-19 01:17:33 +0700

    add module property items

---
+++ app/Http/Controllers/API/v1/Eks/PropertyItemController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Eks;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\API\v1\PropertyItem\CreateRequest;
+use App\Models\PropertyItem;
+use App\Models\PropertyType;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class PropertyItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $limit = $request->input('limit') ?: 10;
+        $propertyItems = PropertyItem::getLists($request)->paginate($limit);
+        $propertyItems->transform(function ($propItem) {
+            $items = $propItem->toArray();
+            $items['photos'] = $propItem->photos->transform(function ($photo) {
+                return $photo->image;
+            });
+            return $items;
+        });
+        return response()->success(['contents' => $propertyItems]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateRequest $request)
+    {
+        \DB::beginTransaction();
+        try {
+            $propertyItem = PropertyItem::create($request->all());
+            $status = 'success'; $message = "Project Item {$propertyItem->name} berhasil disimpan.";
+            $code = 201;
+            \DB::commit();
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            $status = 'error'; $message = "Project Item {$request->input('name')} gagal disimpan.";
+            $code = 500;
+        }
+        return response()->{$status}(compact('message'), $code);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PropertyItem $property_item)
+    {
+        $prop = $property_item->load('photos')->toArray();
+        $prop['photos'] = $property_item->photos->transform(function ($photo) {
+            return $photo->image;
+        });
+        return response()->success(['contents' => $prop]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showPropertyType($property_type, PropertyItem $property_item)
+    {
+        $propType = PropertyType::findBySlug($property_type);
+
+        if ($propType->id === $property_item->property_type_id)
+            return $this->show($property_item);
+
+        throw new ModelNotFoundException;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CreateRequest $request, PropertyItem $property_item)
+    {
+        \DB::beginTransaction();
+        try {
+            $property_item->update($request->all());
+            $status = 'success'; $message = "Project Item {$property_item->name} berhasil dirubah.";
+            $code = 200;
+            \DB::commit();
+        } catch (\Exception $e) {
+            \DB::rollBack();
+            $status = 'error'; $message = "Project Item {$request->input('name')} gagal dirubah.";
+            $code = 500;
+        }
+        return response()->{$status}(compact('message'), $code);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updatePropertyType(CreateRequest $request, $property_type, PropertyItem $property_item)
+    {
+        $propType = PropertyType::findBySlug($property_type);
+
+        if ($propType->id === $property_item->property_type_id)
+            return $this->update($request, $property_item);
+
+        throw new ModelNotFoundException;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/eks.php
+++ routes/eks.php
@@ -24,11 +24,88 @@ Route::group( [ 'prefix' => 'v1/eks', 'namespace' => 'API\v1\Eks' ], function() 
 
 	// route that required for login
 	Route::group( [ 'middleware' => [ 'api.auth' ] ], function () {
+
 		Route::group( [ 'prefix' => 'auth' ], function () {
 			Route::post( 'register-simple', 'AuthController@update' );
 			Route::post( 'register-complete', 'AuthController@update' );
 			Route::delete( 'logout', 'AuthController@destroy' );
 		} );
+
+		/**
+		 * Route property
+		 */
+		Route::resource('property', 'PropertyController', [
+			'except' => [ 'create', 'edit' ],
+		]);
+
+		/**
+		 * Route property type get by property
+		 */
+		Route::group([
+			'as' => 'property.',
+			'prefix' => 'property/{property}',
+			'middleware' => [ 'property.access' ]
+		], function() {
+
+			Route::get('property-type', 'PropertyTypeController@index')
+				->name('property-type.index');
+
+			Route::get('property-type/{property_type}', 'PropertyTypeController@showProperty')
+				->name('property-type.show');
+
+			Route::match(['put', 'patch'], 'property-type/{property_type}', 'PropertyTypeController@updateProperty')
+				->name('property-type.update');
+		});
+
+		/**
+		 * Route property type
+		 */
+		Route::resource('property-type', 'PropertyTypeController', [
+			'except' => [ 'create', 'edit' ],
+		]);
+
+		/**
+		 * Route property type
+		 */
+		Route::resource('property-item', 'PropertyItemController', [
+			'except' => [ 'create', 'edit' ],
+		]);
+
+		/**
+		 * Route property type
+		 */
+		Route::group([
+			'as' => 'property-type.',
+			'prefix' => 'property-type/{property_type}',
+			'middleware' => [ 'property-type.access' ]
+		], function () {
+
+			Route::get('property-item', 'PropertyItemController@index')
+				->name('property-item.index');
+
+			Route::get('property-item/{property_item}', 'PropertyItemController@showPropertyType')
+				->name('property-item.show');
+
+			Route::match(['put', 'patch'], 'property-item/{property_item}', 'PropertyItemController@updatePropertyType')
+				->name('property-item.update');
+		});
+
+		/**
+		 * Route group profile
+		 */
+		Route::group(['prefix' => 'profile'], function () {
+
+			/**
+			 * Route for get simple profile
+			 */
+			Route::get('/', 'ProfileController@index');
+
+			/**
+			 * Route for update profile
+			 */
+			Route::match(['put', 'patch'], 'update', 'ProfileController@update');
+		});
+
 	} );
 
 	/**
@@ -40,45 +117,5 @@ Route::group( [ 'prefix' => 'v1/eks', 'namespace' => 'API\v1\Eks' ], function() 
 		 * Route for reset and send new password
 		 */
 		Route::post('reset', 'PasswordController@reset');
-	});
-
-	/**
-	 * Route property
-	 */
-	Route::resource('property', 'PropertyController', [
-		'except' => [ 'create', 'edit' ],
-		'middleware' => [ 'api.auth' ]
-	]);
-
-	/**
-	 * Route property
-	 */
-	Route::resource('property/{property}/property-type', 'PropertyTypeController', [
-		'except' => [ 'create', 'edit' ],
-		'middleware' => [ 'api.auth', 'property.access' ]
-	]);
-
-	/**
-	 * Route property
-	 */
-	Route::resource('property-type', 'PropertyTypeController', [
-		'except' => [ 'create', 'edit' ],
-		'middleware' => [ 'api.auth' ]
-	]);
-
-	/**
-	 * Route group profile
-	 */
-	Route::group(['prefix' => 'profile', 'middleware' => ['api.auth'] ], function () {
-
-		/**
-		 * Route for get simple profile
-		 */
-		Route::get('/', 'ProfileController@index');
-
-		/**
-		 * Route for update profile
-		 */
-		Route::match(['put', 'patch'], 'update', 'ProfileController@update');
 	});
 });
