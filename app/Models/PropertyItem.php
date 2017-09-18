70617494287e90f1d333239254dd8b18dc30d2bf
Candra Sudirman <candra.s@smooets.com>
2017-09-19 01:14:47 +0700

    add query for get index and search property items

--- app/Models/PropertyItem.php
+++ app/Models/PropertyItem.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class PropertyItem extends Model
 {
@@ -43,5 +44,62 @@ class PropertyItem extends Model
     public function photos()
     {
         return $this->morphMany( Photo::class, 'photoable' );
+    }
+
+    /**
+     * Get lists of property type
+     * 
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param  Request $request [description]
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGetLists($query, Request $request)
+    {
+        $sort = $request->input('sort') ? explode('|', $request->input('sort')) : ['id', 'asc'];
+
+        return $query
+            ->with('photos')
+            ->where(function ($propertyType) use (&$request) {
+                if ($request->has('property_type_id')) 
+                    $propertyType->where('property_type_id', $request->input('property_type_id'));
+
+                if ($request->has('is_available')) 
+                    $propertyType->where('is_available', $request->input('is_available'));
+
+                if ($request->has('status')) 
+                    $propertyType->where('status', $request->input('status'));
+
+                if ($request->has('price')) 
+                    $propertyType->whereBetween('price', explode('|', $request->input('price')));
+            })
+            ->where(function ($propertyType) use (&$request, &$query) {
+                if ($request->has('search')) $query->search($request);
+            })
+            ->whereHas('propertyType', function ($type) use (&$request) {
+                if ($user = $request->user()) {
+                    if ($user->inRole('developer')) {
+                        $developerId = $user->developer->id;
+                        $type->developerOwned($developerId);
+                    }
+                }
+            })
+            ->select(array_merge(['id'], $this->fillable))
+            ->orderBy($sort[0], $sort[1]);
+    }
+
+    /**
+     * Scope a query for search user.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeSearch($query, Request $request)
+    {
+        return $query
+            ->where('certificate', 'ilike', "%{$request->input('search')}%")
+            ->orWhere('surface_area', 'ilike', "%{$request->input('search')}%")
+            ->orWhere('building_area', 'ilike', "%{$request->input('search')}%")
+            ->orWhere('name', 'ilike', "%{$request->input('search')}%");
     }
 }
