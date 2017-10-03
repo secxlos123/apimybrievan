330257efa3f37af62abc26b74f116ff3e485161c
Luthfan H <luthfan.h@smooets.com>
2017-10-03 12:25:12 +0700

    fix migration

--- database/migrations/2017_10_02_103135_add_some_field_for_visits_reports.php
+++ database/migrations/2017_10_02_103135_add_some_field_for_visits_reports.php
@@ -13,7 +13,7 @@ class AddSomeFieldForVisitsReports extends Migration
      */
     public function up()
     {
-        Schema::create( 'visit_reports', function ( Blueprint $table ) {
+        Schema::table('visit_reports', function (Blueprint $table) {
             $table->integer( 'npwp' )->nullable();
             $table->string( 'income_type' )->nullable();
             $table->string( 'couples_monthly_salary' )->nullable();
@@ -28,6 +28,7 @@ class AddSomeFieldForVisitsReports extends Migration
             $table->integer( 'use_reason_value' )->nullable();
             $table->string( 'use_reason' )->nullable();
             $table->integer( 'id_prescreening')->nullable();
+        });
     }
 
     /**
@@ -37,6 +38,21 @@ class AddSomeFieldForVisitsReports extends Migration
      */
     public function down()
     {
-        //
+        Schema::table( 'visit_reports', function ( Blueprint $table ) {
+            $table->dropColumn( 'npwp' );
+            $table->dropColumn( 'income_type' );
+            $table->dropColumn( 'couples_monthly_salary' );
+            $table->dropColumn( 'other_income_couples' );
+            $table->dropColumn( 'kpp_type' );
+            $table->dropColumn( 'financed_type' );
+            $table->dropColumn( 'sector_economy' );
+            $table->dropColumn( 'project_value' );
+            $table->dropColumn( 'program_value' );
+            $table->dropColumn( 'sub_third_party_value' );
+            $table->dropColumn( 'family_name' );
+            $table->dropColumn( 'use_reason_value' );
+            $table->dropColumn( 'use_reason' );
+            $table->dropColumn( 'id_prescreening');
+        } );
     }
 }
