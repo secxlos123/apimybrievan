19ec9d332004c2a3201aa08bf80b4d080627ef72
Luthfan H <luthfan.h@smooets.com>
2017-10-03 10:09:57 +0700

    migration for table visits reports

---
+++ database/migrations/2017_10_02_103135_add_some_field_for_visits_reports.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSomeFieldForVisitsReports extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create( 'visit_reports', function ( Blueprint $table ) {
+            $table->integer( 'npwp' )->nullable();
+            $table->string( 'income_type' )->nullable();
+            $table->string( 'couples_monthly_salary' )->nullable();
+            $table->string( 'other_income_couples' )->nullable();
+            $table->string( 'kpp_type' )->nullable();
+            $table->integer( 'financed_type' )->nullable();
+            $table->integer( 'sector_economy' )->nullable();
+            $table->integer( 'project_value' )->nullable();
+            $table->integer( 'program_value' )->nullable();
+            $table->integer( 'sub_third_party_value' )->nullable();
+            $table->string( 'family_name' )->nullable();
+            $table->integer( 'use_reason_value' )->nullable();
+            $table->string( 'use_reason' )->nullable();
+            $table->integer( 'id_prescreening')->nullable();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
