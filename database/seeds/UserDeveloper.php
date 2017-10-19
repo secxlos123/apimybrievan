d0017fcc0c8051e57b7070a3c6cd0492e8b52a2e
Erwan Akse <erwanakse@gmail.com>
2017-10-19 14:28:33 +0700

    change Seed Independent

--- database/seeds/UserDeveloper.php
+++ database/seeds/UserDeveloper.php
@@ -14,34 +14,44 @@ class UserDeveloper extends Seeder
         DB::beginTransaction();
         try {
 
-        		 	$id = DB::table('developers')->insertGetId(array(
-        		 	'dev_id_bri' => 1, 
-        		 	'company_name' => "TIDAK ADA",
-        			'created_by'=>	"",
-        			'pks_number'=> "",
-        			'plafond'=>"0",
-        			'address'=>"",
-        			'summary'=>"",
-        			'is_approved'=> "t"
-        		));
-					DB::table('properties')->insert(array(
-        		  	'developer_id' => $id,		 	 
-        		  	'name' => "TIDAK MENGIKUTI PROJECT",
-        		  	'pic_name'=>"",
-        		  	'pic_phone'=>"",
-        		  	'address'=>"",
-        		  	'category'=>0,
-        		  	'latitude'=>"",
-        		  	'longitude'=>"",
-        		  	'description'=>"",
-        		  	'facilities'=>"",
-        		  	'slug'=>0,
-        		  	'is_approved'=> "t"
-        		  ));
+        		 	$user = APP\Models\User::create(array(
+        		 	'email' => 'mybri@bri.co.id',
+                    'password'=> bcrypt('12345678'),
+                    'first_name'=>'INDEPENDENT',
+                    'last_name' => 'BRI'
+                    ));
+                    $user->roles()->attach(4);
+                    $activation = Activation::create($user);
+                    Activation::complete($user, $activation->code);
+
+                    $city = App\Models\City::all()->random();
+                    $developer = App\Models\Developer::create(array(
+                        'user_id' => $user->id,
+                        'dev_id_bri' => 1,  
+                        'company_name' => "INDEPENDENT", 
+                        'created_by'=>  "", 
+                        'pks_number'=> "", 
+                        'plafond'=>"0", 
+                        'address'=>"", 
+                        'summary'=>"", 
+                        'is_approved'=> "t" 
+
+                    ));
+                    $properties = App\Models\Property::create(array(
+                    'developer_id' => $developer->id,           
+                    'name' => "INDEPENDENT",
+                    'pic_name'=>"",
+                    'pic_phone'=>"",
+                    'address'=>"",
+                    'category'=>0,
+                    'latitude'=>"",
+                    'longitude'=>"",
+                    'description'=>"",
+                    'facilities'=>"",
+                    'slug'=>0,
+                    'is_approved'=> "t"
+                    ));
         		 
-        		 
-        		
-       
         	DB::commit();
         } catch (Exception $e) {
         	dd($e);
