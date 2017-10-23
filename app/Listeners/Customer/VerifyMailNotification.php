99d4a826a79433a681cee9b991ea5fd416f56f2c
Erwan Akse <erwanakse@gmail.com>
2017-10-23 08:46:27 +0700

    add validation & fix template mail customer

--- app/Listeners/Customer/VerifyMailNotification.php
+++ app/Listeners/Customer/VerifyMailNotification.php
@@ -32,38 +32,6 @@ class VerifyMailNotification
         $customer = $event->customer->detail;
         $customer_data= $event->customer;
         $eform = $event->eform;
-        $nikah = '';
-        $tempat= '';
-
-        switch ($customer->status) {
-            case 1:
-                 $nikah = 'Belum Menikah';
-                break;
-            case 2:
-                $nikah = 'Menikah';
-                break;
-            case 3:
-                $nikah = 'Janda/Duda';
-                break;
-            default:
-                # code...
-                break;
-        }
-
-        switch ($customer->address_status) {
-            case '0':
-                 $tempat = 'Milik Sendiri';
-                break;
-            case '1':
-                $tempat = 'Milik Orang Tua/Mertua atau Rumah Dinas';
-                break;
-            case '3':
-                $tempat = 'Tinggal di Rumah Kontrakan';
-                break;
-            default:
-                # code...
-                break;
-        }
 
         $mail = [
             'email' => $customer_data->email,
@@ -71,18 +39,19 @@ class VerifyMailNotification
             'url' => env( 'MAIN_APP_URL', 'https://mybri.stagingapps.net' ) . '/eform/' . $eform->token,
             'nik' => $customer->nik,
             'address'=> $customer->address,
+            'city_id'=> $customer->city->name,
             'phone'=> $customer_data->phone,
             'mobile_phone'=>$customer_data->mobile_phone,
             'mother_name'=>$customer->mother_name,
             'birth_place_id'=> $customer->birth_place_city->name,
-            'identity'=> $customer->identity,
+            'identity'=> $customer->Identity,
             'gender'=>$customer_data->gender,
-            'status'=>$nikah,
-            'address_status'=>$tempat,
-            'citizenship_id'=>$customer->citizenship_id,
+            'status'=>$customer->Status,
+            'address_status'=> $customer->StatusAddress,
+            'citizenship_id'=>$customer->Citizenship,
             'couple_nik'=>$customer->couple_nik,
             'couple_name'=>$customer->couple_name,
-            'couple_identity'=>$customer->couple_identity,
+            'couple_identity'=>$customer->CoupleIdentity,
             'couple_birth_place_id'=> $customer->couple_birth_place_city->name,
             'job_field_id'=>$customer->job_field_id,
             'job_type_id'=>$customer->job_type_id,
