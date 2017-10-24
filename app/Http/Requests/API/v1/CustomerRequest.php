bc0083d33281415677e8fc39e37a32938cb9466a
Erwan Akse <erwanakse@gmail.com>
2017-10-24 11:00:56 +0700

    change mandatory identify

--- app/Http/Requests/API/v1/CustomerRequest.php
+++ app/Http/Requests/API/v1/CustomerRequest.php
@@ -82,7 +82,7 @@ class CustomerRequest extends BaseRequest
                         'emergency_name'=>'required_if:verify_status,verify',
                         'emergency_mobile_phone'=>'required_if:verify_status,verify',
                         'emergency_relation'=>'required_if:verify_status,verify',
-                        'identity'=>'required_if:verify_status,verify|image|mimes:jpg,jpeg,png',
+                        'identity'=>'image|mimes:jpg,jpeg,png',
                         'first_name'=>'',
                         'last_name'=>'',
                         'verify_status'=>'required|in:verify,verified',
