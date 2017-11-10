1ad3ee981b4d7be02ca7ebc6ec7cee7678544cde
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2017-11-10 19:06:11 +0700

    fix coleteral

--- app/Models/Collateral.php
+++ app/Models/Collateral.php
@@ -25,28 +25,27 @@ class Collateral extends Model
         $sort = $request->input('sort') ? explode('|', $request->input('sort')) : ['prop_id', 'asc'];
 
         return $query->from('developer_properties_view_table')
-            ->where(function ($property) use (&$request, &$query) {
-
-                /**
-                 * Query for search developers.
-                 */
-                if ($request->has('search')) $query->search($request);
-            })
             ->where(function ($property) use ($request) {
 
-                /**
-                 * Query for filter by city_id.
-                 */
-                if ($request->has('city_id')) $developer->where('city_id', $request->input('city_id'));
-
-                /**
-                 * Query for filter by range project.
-                 */
-                if ($request->has('project')) $developer->whereBetween('project', explode('|', $request->input('project')));
+                if ($request->has('city_id')) $developer->where('prop_city_id', $request->input('city_id'));
 
             })
             ->select('*')
             ->where('prop_id', '!=', '1')
             ->orderBy($sort[0], $sort[1]);
+    }
+
+    /**
+     * Scope a query to get lists of roles.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGetDetails($query, $id)
+    {
+
+        return $query->from('developer_properties_view_table')
+            ->where('prop_id', '=', $id)->get();
     }
 }
