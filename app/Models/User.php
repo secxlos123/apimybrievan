a0e4349c7c30e03389d68058c099871eb16b0d05
Erwan Akse <erwanakse@gmail.com>
2017-11-28 12:14:42 +0700

    add friendly generate number

--- app/Models/User.php
+++ app/Models/User.php
@@ -266,7 +266,8 @@ class User extends Authenticatable
     protected function createOrUpdate(Request $request, $user, $relation = null)
     {
         if ( ! $user instanceof $this ) {
-            $password = str_random(8);
+            //$password = str_random(8);
+            $password = $this->randomPassword(8,"lower_case,upper_case,numbers");
 	        \Log::info('================================== Password ============================');
 	        \Log::info($password);
             $request->merge(['password' => bcrypt($password)]);
@@ -638,5 +639,35 @@ class User extends Authenticatable
         return $return;
 
 
+    }
+    /**
+     * Generate Random Password
+     * @param  [type] $length     [description]
+     * @param  [type] $characters [description]
+     * @return [type]             [description]
+     */
+    public function randomPassword($length,$characters) {
+        // $length - the length of the generated password
+        // $characters - types of characters to be used in the password
+        // define variables used within the function
+        $symbols = array();
+        $passwords = array();
+        $used_symbols = '';
+        $pass = '';
+        // an array of different character types
+        $symbols["lower_case"] = 'abcdefghijklmnopqrstuvwxyz';
+        $symbols["upper_case"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $symbols["numbers"] = '1234567890';
+        $symbols["special_symbols"] = '!?~@#-_+<>[]{}';
+        $characters = explode(",",$characters); // get characters types to be used for the passsword
+        foreach ($characters as $key=>$value) {
+            $used_symbols .= $symbols[$value]; // build a string with all characters
+        }
+        $symbols_length = strlen($used_symbols) - 1; //strlen starts from 0 so to get number of characters deduct 1
+            for ($i = 0; $i < $length; $i++) {
+                $n = rand(0, $symbols_length); // get a random character from the string with all characters
+                $pass .= $used_symbols[$n]; // add the character to the password string
+            }
+        return $pass; // return the generated password
     }
 }
