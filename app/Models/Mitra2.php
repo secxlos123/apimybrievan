ea9665d3ea6ad01b1d964f5b9a3a79811d5906b6
secxlos <evan.tohap@lawencom.com>
2017-11-30 17:55:22 +0700

    update create

---
+++ app/Models/Mitra2.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;;
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Builder;
+
+use Illuminate\Http\Request;
+use Sentinel;
+use Asmx;
+use RestwsHc;
+use Cartalyst\Sentinel\Users\EloquentUser as Authenticatable;
+
+
+
+class Mitra2 extends Model
+{
+	
+	protected $table = 'mitra';
+	
+	  public function scopeFilter( $query, Request $request )
+    {
+      $mitra = $query->where( function( $mitra ) use( $request, &$user ) {
+			
+      $kode = $request->input('key');
+                    $mitra->Where('mitra.NAMA_INSTANSI','like', $kode.'%');
+        } );
+
+				$mitra = $mitra->select([
+                    'mitra.BRANCH_CODE','mitra.NAMA_INSTANSI','mitra.idMitrakerja','mitra.segmen','mitra.UNIT_KERJA',
+         		 \DB::Raw(" case when mitra.kode is not null then 2 else 1 end as new_order ")
+                ]);
+
+        \Log::info($mitra->toSql());
+
+        return $mitra;
+
+    }
+}
