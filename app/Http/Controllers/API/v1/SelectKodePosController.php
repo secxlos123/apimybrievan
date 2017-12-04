30307baf768dddf5e4aa604ad386de06cf2bfcb2
secxlos <evan.tohap@lawencom.com>
2017-12-04 20:06:26 +0700

    update route create KodePos.php

---
+++ app/Http/Controllers/API/v1/SelectKodePosController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\KodePos;
+use Sentinel;
+use DB;
+
+class SelectKodePosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+	public function SelectKodePos( Request $request )
+	{
+        \Log::info($request->all());
+        $kodepos = KodePos::filter( $request )->get();
+		$kodedrkode = '';			
+	
+		foreach($kodepos as $key){
+			if($kodedrkode!=$key['postal_code']){
+			$kodepost[]['postal_code'] = $key['postal_code'];
+			$kodedrkode = $key['postal_code'];
+			}
+		}
+
+        return response()->success( [
+            'message' => 'Sukses',
+            'contents' => $kodepost
+        ], 200 );
+
+	}
+
+
+}
