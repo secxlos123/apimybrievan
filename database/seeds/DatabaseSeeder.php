507fcb1aff7f0c719345a8c693785b5d1ddfc380
Aswin <csbcan57@gmail.com>
2017-12-04 18:31:50 +0700

    commit DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call( UsersTableSeeder::class );
-        $this->call( MitraTableSeeder::class );
+        //$this->call( MitraTableSeeder::class );
 		$this->call( KodeposTableSeeder::class );
         $this->call( RolesTableSeeder::class );
         $this->call( CityTableSeeder::class );
