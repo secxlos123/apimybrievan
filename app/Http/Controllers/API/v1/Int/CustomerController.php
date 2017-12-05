775a5e0f02b5636b6b6549ce94656f58e2eb367d
Erwan Akse <erwanakse@gmail.com>
2017-12-05 15:03:15 +0700

    add handle update kpr on verification

--- app/Http/Controllers/API/v1/Int/CustomerController.php
+++ app/Http/Controllers/API/v1/Int/CustomerController.php
@@ -11,6 +11,7 @@ use App\Models\Customer;
 use App\Models\CustomerDetail;
 use App\Models\EForm;
 use App\Models\User;
+use App\Models\KPR;
 use Sentinel;
 use DB;
 
@@ -126,7 +127,10 @@ class CustomerController extends Controller
 	{
 		DB::beginTransaction();
 		$customer = Customer::findOrFail( $id );
-		$customer->verify( $request->except('join_income') );
+		if ($request->has('eform_id')) {
+		KPR::updateOrCreate(['eform_id' => $request->eform_id],$request->only('status_property','developer_id', 'property_id', 'price', 'building_area', 'home_location', 'year', 'active_kpr', 'dp', 'request_amount', 'developer_name', 'property_name', 'kpr_type_property','property_type','property_type_name','property_item','property_item_name'));
+		}
+		$customer->verify( $request->except('join_income','status_property', 'eform_id', 'developer_id', 'property_id', 'price', 'building_area', 'home_location', 'year', 'active_kpr', 'dp', 'request_amount', 'developer_name', 'property_name', 'kpr_type_property','property_type','property_type_name','property_item','property_item_name') );
 		$eform = EForm::generateToken( $customer->personal['user_id'] );
 
 		DB::commit();
