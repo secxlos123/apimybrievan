e5d0e02101098e27fc3fa08a85589d32cfef2670
secxlos <evan.tohap@lawencom.com>
2017-12-06 16:43:04 +0700

    tester suratrekomendasi

--- app/Mail/suratrekomendasi.php
+++ app/Mail/suratrekomendasi.php
@@ -31,7 +31,14 @@ class suratrekomendasi extends Mailable
     {
 		$file = storage_path('app/PDF/Surat_Kuasa_Potong_Upah.pdf');
 		$file2 = storage_path('app/PDF/Surat_Rekomendasi_Atasan.pdf');
-		 $k = $this->view('mails.suratrekomendasi');
+		 $k = $this->view('mails.suratrekomendasi')->attach($file, [
+        'as' => 'Surat Kuasa Potong Gaji',
+        'mime' => 'application/.pdf',
+    ])->attach($file2, [
+        'as' => 'Surat Rekomendasi',
+        'mime' => 'application/.pdf',
+    ]);
+return $k;
  //       if (env('APP_ENV') == 'production') {
  //           return $this->view( 'mails.example', $this->mail );
  //       }
