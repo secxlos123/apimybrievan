e2ce65f9bc6a9ba93912b46b65af719102c229e3
firmanmujahidin <Firmanmujahidin@gmail.com>
2017-12-11 16:29:24 +0700

    add Role FO and MOdel MarketingController

---
+++ database/migrations/2017_12_11_040920_create_crm_activity_type_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Crm\ActivityType;
+
+class CreateCrmActivityTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('crm_activity_types', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('activity_name');
+          $table->timestamps();
+      });
+
+      // $activity_names = ['Pick up Service', 'TOP', 'CMS', 'Akuisisi'];
+      //   foreach ($activity_names as $activity_name){
+	    //     ActivityType::create(['activity_name' => $activity_name, 'created_at' => \Carbon\Carbon::now()->toDateTimeString(), 'updated_at' => \Carbon\Carbon::now()->toDateTimeString()]);
+      //   }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
