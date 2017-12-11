4cb76026ac00aa59c7c3525abc0f7a9c532fb7e9
Muhammad Bima <muhammadbima20@gmail.com>
2017-12-11 09:24:37 +0700

    API List User Dasboard Developers

--- app/Http/Controllers/API/v1/Eks/DashboardController.php
+++ app/Http/Controllers/API/v1/Eks/DashboardController.php
@@ -4,22 +4,25 @@ namespace App\Http\Controllers\API\v1\Eks;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\UserDeveloper;
+use App\Models\User;
 class DashboardController extends Controller
 {
     protected $request;
-    protected $property;
+    protected $modelUser;
 
     public function __construct(Request $request)
     {
-        $this->request = $request;
-        $this->property = new UserDeveloper;
+        $this->request   = $request;
+        $this->modelUser = new User;
     }
 
     public function dashboard()
     {
-    	$userList  = $this->property->getListUserDeveloper();
-    	$chartData = $this->property->getDataChart();
+    	$params    = $this->request->all();
+    	$start 	   = (isset($params['start'])) ? $params['start'] : '';
+    	$end 	   = (isset($params['end'])) ? $params['end'] : '';
+    	$userList  = $this->modelUser->getListUserProperties($start, $end);
+    	$chartData = "";
         return response()->success([
             'contents' => [
                 'user_list' => $userList,
