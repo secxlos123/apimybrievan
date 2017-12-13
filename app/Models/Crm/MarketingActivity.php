336b4c657900588a04056ac9839ea2f709758ead
firmanmujahidin <Firmanmujahidin@gmail.com>
2017-12-14 06:35:47 +0700

    add fillable row

--- app/Models/Crm/MarketingActivity.php
+++ app/Models/Crm/MarketingActivity.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class MarketingActivity extends Model
 {
-    //
+    protected $fillable = [
+      'pn',
+      'object_activity',
+      'action_activity',
+      'start_date',
+      'end_date',
+      'longitude',
+      'latitude',
+      'marketing_id',
+      'pn_join',
+      'desc'
+    ];
 }
