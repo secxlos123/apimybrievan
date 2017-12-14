f9bf3f80589e4ee52ac9151cce9a6a5220d44de5
Aswin <csbcan57@gmail.com>
2017-12-14 22:11:47 +0700

    add field briguna

---
+++ database/migrations/2017_12_14_145101_add_evan.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEvan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'briguna', function ( Blueprint $table ) {
+            $table->text( 'jumlah_pekerja')->nullable();
+            $table->text( 'jumlah_debitur')->nullable();         
+            $table->text( 'scoring_mitra')->nullable();           
+        } );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
