7ec5f0a60dae4d1c072a50735cd3a75fa853a0fd
secxlos <evan.tohap@lawencom.com>
2017-12-14 15:43:19 +0700

    update

--- app/Http/Controllers/API/v1/SelectCabangController.php
+++ app/Http/Controllers/API/v1/SelectCabangController.php
@@ -94,8 +94,8 @@ class SelectCabangController extends Controller
     private function fetch(Request $request)
     {
         \Log::info($request->all());
-        $long = number_format($request->get('long', 106.813880), 5);
-        $lat = number_format($request->get('lat', -6.217458), 5);
+        $long = number_format($request->get('long', 106.8223912), 5);
+        $lat = number_format($request->get('lat', -6.302579), 5);
         $return = RestwsHc::setBody([
             'request' => json_encode([
                 'requestMethod' => 'get_near_branch_v2',
