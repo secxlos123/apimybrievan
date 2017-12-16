2c9d562318c43924ec37e2ff2ae9545017b3666a
secxlos <evan.tohap@lawencom.com>
2017-12-16 17:20:39 +0700

    update

--- app/Models/KodePos.php
+++ app/Models/KodePos.php
@@ -8,16 +8,16 @@ class KodePos extends Model
 {
 	  protected $table = 'tbl_kodepos';
 	  public function scopeFilter($query, $request) {
+		  if($request->has($request['key'])){
         $kodepos = $query->where( function( $kodepos ) use( $request, &$user ) {
           $kode = $request['key'];
           $kodepos->Where('tbl_kodepos.postal_code','like', $kode.'%');
 					$kodepos->orWhere('tbl_kodepos.postal_code','like', $kode.'%');
 					$kodepos->orderByRaw('tbl_kodepos.postal_code DESC');
         });
+		  };
 
-				$kodepos = $kodepos->select([
-          'postal_code','Kelurahan','Kecamatan','Kota','Propinsi',
-       		\DB::Raw(" case when tbl_kodepos.postal_code is not null then 2 else 1 end as new_order ")
+				$kodepos = $kodepos->select(['*')
         ]);
 
         \Log::info($kodepos->toSql());
