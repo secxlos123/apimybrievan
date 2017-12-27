506649893239b5ab78270cad8930b479796ac8bc
secxlos <evan.tohap@lawencom.com>
2017-12-27 10:40:22 +0700

    update

---
+++ app/Http/Controllers/API/v1/Dir_rpcController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\API\v1\DirrpcRequest;
+use App\Models\User;
+use App\Models\DIRRPC;
+use App\Models\UserServices;
+use DB;
+
+class Dir_rpcController extends Controller
+{
+    public function __construct(User $user, UserServices $userservices)
+    {
+        $this->user = $user;
+        $this->userservices = $userservices;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index( Request $request )
+    {
+        \Log::info($request->all());
+        $limit = $request->input( 'limit' ) ?: 10;
+        $newDir = DIRRPC::filter( $request )->paginate( $limit );
+        return response()->success( [
+            'message' => 'Sukses',
+            'contents' => $newDir
+        ], 200 );
+    }
+}
