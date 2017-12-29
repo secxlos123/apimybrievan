aab2aa109a7fd6bd3d3fbe79e7ea7131be008992
Erwan Akse <erwanakse@gmail.com>
2017-12-29 15:52:37 +0700

    fix validation ots document

--- app/Http/Requests/API/v1/Collateral/CreateOtsDoc.php
+++ app/Http/Requests/API/v1/Collateral/CreateOtsDoc.php
@@ -24,19 +24,19 @@ class CreateOtsDoc extends FormRequest
     public function rules()
     {
         return [
-        'collateral_binding_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'collateral_insurance_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'life_insurance_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'ownership_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'building_permit_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'sales_law_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'property_tax_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'sale_value_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'progress_one_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'progress_two_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'progress_three_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'progress_four_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf',
-        'progress_five_doc'=>'required|mimes:jpeg,png,jpg,zip,pdf'
+        'collateral_binding_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'collateral_insurance_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'life_insurance_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'ownership_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'building_permit_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'sales_law_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'property_tax_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'sale_value_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'progress_one_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'progress_two_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'progress_three_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'progress_four_doc'=>'mimes:jpeg,png,jpg,zip,pdf',
+        'progress_five_doc'=>'mimes:jpeg,png,jpg,zip,pdf'
         ];
     }
 }
