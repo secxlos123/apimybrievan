b30f8b9f09a8caeb0e77188827c3835e64b83a3a
secxlos <evan.tohap@lawencom.com>
2018-01-04 10:47:55 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::group(['prefix' => 'v1', 'namespace' => 'API\v1'], function () {
 		Route::post('SelectMitra', 'SelectMitraController@SelectMitra');
 		Route::post('SelectKodePos', 'SelectKodePosController@SelectKodePos');
 		Route::post('SelectCabang', 'SelectCabangController@getCabangMitra');
+		Route::post('getToken', 'SelectUkerController@getToken');
 		Route::post('gimmick_list', 'Int\GimmickController@list_gimmick');
 		Route::post('GetView', 'ViewController@index');
 
