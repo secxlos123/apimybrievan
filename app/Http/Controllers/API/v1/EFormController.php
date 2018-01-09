36e873f90ab3a9825c77e341d57b193fb30667f9
Muhammad Bima <muhammadbima20@gmail.com>
2018-01-09 16:20:55 +0700

    Mobile Notification

--- app/Http/Controllers/API/v1/EFormController.php
+++ app/Http/Controllers/API/v1/EFormController.php
@@ -466,18 +466,10 @@ class EFormController extends Controller
                 ], 422 );
         }
 
-        // //  Push Notification To Pinca
-
-        $notificationIsRead =  $this->userNotification->where('eform_id',$kpr['id'])
-                                       ->whereNull('read_at')
-                                       ->first();
-        if(@$notificationIsRead){
-            $notificationIsRead->markAsRead();
-        }
 
         $userId = CustomerDetail::where('nik', $baseRequest['nik'])->first();
         $usersModel = User::FindOrFail($userId['user_id']);     /*send notification*/
-        // if($role == 'nasabah'){
+        // if($role == 'customer'){
             // $notificationBuilder = new PayloadNotificationBuilder('EForm Notification');
             // $notificationBuilder->setBody('Pengajuan KPR Baru')
             //                     ->setSound('default');
@@ -633,7 +625,7 @@ class EFormController extends Controller
         // $notification = $notificationBuilder->build();
 
         // $topic = new Topics();
-        // $topic->topic('testing')->orTopic('ao_'.$ao_id)->orTopic($pn);
+        // $topic->topic('testing')->orTopic('ao_'.$ao_id);
 
         // $topicResponse = FCM::sendToTopic($topic, null, $notification, null);
         // $topicResponse->isSuccess();
@@ -663,9 +655,9 @@ class EFormController extends Controller
         $user_login = \RestwsHc::getUser();
         if(isset($user_login)){
             $baseRequest['pinca_name'] = $user_login['name'];
-            $baseRequest['pinca_position'] = $user_login['position'];            
-        }        
-  
+            $baseRequest['pinca_position'] = $user_login['position'];
+        }
+
         $eform = EForm::approve( $eform_id, $baseRequest );
         if( $eform['status'] ) {
 
@@ -744,7 +736,7 @@ class EFormController extends Controller
                 /*if(@$notificationIsRead){
                     $notificationIsRead->markAsRead();
                 }*/
-                $usersModel = User::FindOrFail($verify['contents']->user_id);  
+                $usersModel = User::FindOrFail($verify['contents']->user_id);
 
                 if ($status == 'approve') {
                     // $usersModel->notify(new VerificationApproveFormNasabah($verify['contents']));   /*send notification approve*/
