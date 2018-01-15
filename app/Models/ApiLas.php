fcbecbba8ae73a879e326acba7cb50d067ee6520
Aswin <csbcan57@gmail.com>
2018-01-15 11:18:34 +0700

    add field in briguna

--- app/Models/ApiLas.php
+++ app/Models/ApiLas.php
@@ -32,12 +32,13 @@ class ApiLas extends Model
 
     public function eform_briguna() {
         $eforms = DB::table('eforms')
-                 ->select('eforms.ref_number','eforms.created_at', 'eforms.ao_id',
-                    'eforms.ao_name','eforms.ao_position','briguna.id','briguna.id_aplikasi',
-                    'briguna.request_amount','briguna.Plafond_usulan','briguna.is_send',
-                    'briguna.eform_id','briguna.tp_produk','customer_details.nik',
-                    'customer_details.birth_date','customer_details.address',
-                    'customer_details.mother_name',
+                 ->select('eforms.ref_number','eforms.created_at','eforms.ao_id',
+                    'eforms.ao_name','eforms.ao_position','eforms.pinca_name',
+                    'eforms.pinca_position','briguna.id','briguna.id_aplikasi',
+                    'briguna.no_rekening','briguna.request_amount','briguna.Plafond_usulan',
+                    'briguna.is_send','briguna.eform_id','briguna.tp_produk',
+                    'customer_details.nik','customer_details.birth_date',
+                    'customer_details.address','customer_details.mother_name',
                     'users.first_name','users.mobile_phone','users.gender'
                    )
                  ->join('briguna', 'eforms.id', '=', 'briguna.eform_id')
