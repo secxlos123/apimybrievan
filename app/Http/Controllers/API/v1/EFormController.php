96f6ec045020fa74abb3995bd14a62e847a521b6
secxlos <evan.tohap@lawencom.com>
2018-01-17 11:42:11 +0700

    update

--- app/Http/Controllers/API/v1/EFormController.php
+++ app/Http/Controllers/API/v1/EFormController.php
@@ -73,7 +73,7 @@ class EFormController extends Controller
         \Log::info($request->all());
           $eform = EformBriguna::filter( $request )->get();
 		  $eform = $eform->toArray();
-          $eform[0]['Url'] = 'http://api.dev.net/uploads/';
+          $eform[0]['Url'] = env('APP_URL').'/uploads/';
         return response()->success( [
             'contents' => $eform
         ],200 );
@@ -185,7 +185,7 @@ class EFormController extends Controller
 		  $eform[0]['customer']['schedule'] = [];
 		  $eform[0]['customer']['is_approved'] = $eform[0]['is_approved'];
 
-          $eform[0]['Url'] = 'http://api.dev.net/uploads/';
+          $eform[0]['Url'] = env('APP_URL').'/uploads/';
 
 		  $eform[0]['nominal'] = $eform[0]['request_amount'];
 		  $eform[0]['costumer_name'] = $customer[0]['first_name'].' '.$customer[0]['last_name'];
