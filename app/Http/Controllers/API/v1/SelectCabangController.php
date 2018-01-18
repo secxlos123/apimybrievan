9538e75cf9191567d484495ebc520d9a8916f904
secxlos <evan.tohap@lawencom.com>
2018-01-18 22:23:59 +0700

    update

--- app/Http/Controllers/API/v1/SelectCabangController.php
+++ app/Http/Controllers/API/v1/SelectCabangController.php
@@ -164,7 +164,7 @@ class SelectCabangController extends Controller
                     $search = gettype( strpos($branch['unit_kerja'], $search) ) == 'integer';
                 }
 
-            //    if ( ( $search ) && (( $branch['jenis_uker'] == "KC" ) || ( $branch['jenis_uker'] == "KCP" ) || ( $branch['jenis_uker'] == "BRI UNIT" ) || ( $branch['jenis_uker'] == "KCK" ) )) {
+                if ( ( $search ) && (( $branch['jenis_uker'] == "KC" ) || ( $branch['jenis_uker'] == "KCP" ) || ( $branch['jenis_uker'] == "BRI UNIT" ) || ( $branch['jenis_uker'] == "KCK" ) )) {
 					$countkey = strlen($branch['kode_uker']);
 					$kode_uker =$branch['kode_uker'];
 					//$kode_uker = '';
@@ -202,7 +202,7 @@ class SelectCabangController extends Controller
 						$offices[] = $mitra[$i];
 
 						}
-               // }
+                }
             }
 		}
 
