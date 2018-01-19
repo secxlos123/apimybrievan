367a4eb3fce015a4811ef7f7c9ae256a1fbdd2cc
ranggadarmajati <rangga.android69@gmail.com>
2018-01-19 16:02:14 +0700

    create view table user agen dev

---
+++ database/seeds/ViewUserAgenDeveloperSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ViewUserAgenDeveloperSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::unprepared("DROP VIEW IF EXISTS agen_developers_view_table");
+        \DB::unprepared("CREATE VIEW agen_developers_view_table AS
+          SELECT b.id,
+                 a.id AS user_id,
+                 a.first_name,
+                 a.last_name,
+           		 a.email, concat(a.first_name, ' ', a.last_name) AS full_name,
+            	 a.phone, a.mobile_phone,
+            	 a.is_banned,
+            	 a.last_login,
+            	 b.birth_date,
+            	 b.join_date,
+            	 b.admin_developer_id  
+            	 FROM users a 
+				 LEFT JOIN  user_developers b on a.id = b.user_id");
+    }
+}
