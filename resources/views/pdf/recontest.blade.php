fbb449abe72a83b92c8b54806058b71741230836
Erwan Akse <erwanakse@gmail.com>
2018-01-24 16:44:01 +0700

    change label on recontest pdf

--- resources/views/pdf/recontest.blade.php
+++ resources/views/pdf/recontest.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>E-Recontest PDF</title>
+        <title>E-LKN Recontest PDF</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="" name="description" />
