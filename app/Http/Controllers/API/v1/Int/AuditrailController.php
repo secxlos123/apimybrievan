b8d1429aeef74ba15bcfa90bc308b8bc4e778535
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-01-26 04:44:24 +0700

    create view auditrail pengajuan kredit

---
+++ app/Http/Controllers/API/v1/Int/AuditrailController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Int;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Audit;
+use DB;
+
+class AuditrailController extends Controller
+{
+    public function index(Request $request)
+    {
+    	$limit = $request->input('limit') ?: 10;
+        $auditrail = Audit::getLists($request)->paginate($limit);
+
+        return response()->success(['contents' => $auditrail]);
+    }
+}
