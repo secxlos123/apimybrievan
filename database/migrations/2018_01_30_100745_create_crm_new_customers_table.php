3a6b00898681688206462f2064b2c79f27b8cf1c
Achmad Aliyudin <achmadaliyudin1992@gmail.com>
2018-01-30 12:38:09 +0700

    new costumer

---
+++ database/migrations/2018_01_30_100745_create_crm_new_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCrmNewCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('crm_new_customers', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('pn')->unsigned()->index();
+          $table->integer('branch')->unsigned()->index();
+          $table->string('name');
+          $table->string('nik')->unique();
+          $table->string('email')->unique();
+          $table->string('phone');
+          $table->string('address');
+          $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('crm_new_customers');
+    }
+}
