7591fa7f1b8cce891888cb9820bc34250cd9c8c2
Erwan Akse <erwanakse@gmail.com>
2018-01-31 17:54:42 +0700

    fix collateral image

--- app/Http/Controllers/API/v1/CollateralController.php
+++ app/Http/Controllers/API/v1/CollateralController.php
@@ -201,12 +201,6 @@ class CollateralController extends Controller
           if ( $collateralView ) {
             $eform = EForm::find($collateralView->eform_id);
             if ( $eform ) {
-              $paths = explode('/', $currentPath);
-              $filename = $paths[ count($paths) - 1 ];
-              copy(
-                  public_path( 'uploads/collateral/other/' . $filename )
-                  , public_path( 'uploads/' . $eform->nik . '/' . $image->image_data )
-                );
               foreach( $otsOther->images as $image ) {
                 copy(
                   public_path( 'uploads/collateral/other/' . $otsOther->id . '/' . $image->image_data )
