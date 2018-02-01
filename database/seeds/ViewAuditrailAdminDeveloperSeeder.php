8eb99b9638cddae8cf165393b3b236c670177874
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-02-01 15:00:34 +0700

    fix auditrail agendev show developer name

--- database/seeds/ViewAuditrailAdminDeveloperSeeder.php
+++ database/seeds/ViewAuditrailAdminDeveloperSeeder.php
@@ -14,31 +14,33 @@ class ViewAuditrailAdminDeveloperSeeder extends Seeder
         \DB::unprepared("DROP VIEW IF EXISTS auditrail_admin_developer");
         \DB::unprepared("CREATE VIEW auditrail_admin_developer AS
          select a.id
-         	   , a.created_at 
-         	   , a.action as modul_name
-         	   , a.event
-         	   , a.user_id
-         	   , lower(a.auditable_type) as auditable_type
-         	   , a.url
-         	   , case when b.first_name is not null then concat(b.first_name,' ',b.last_name) else h.name end as username
+               , a.created_at 
+               , a.action as modul_name
+               , a.event
+               , a.user_id
+               , lower(a.auditable_type) as auditable_type
+               , a.url
+               , case when b.first_name is not null then concat(b.first_name,' ',b.last_name) else h.name end as username
                , f.staff_name
-         	   , case when e.slug is not null then e.slug else h.role end as role
-         	   , i.name as project_name
-         	   , c.company_name
-         	   , case when c.id = '1' then 'Non Kerja Sama' when c.id >1 then 'Kerja Sama' else '' end as developer
-         	   , a.old_values
-         	   , a.new_values
-         	   , a.ip_address
-         	   , a.extra_params as action_location
-         	   from audits a
-			   left join users b on b.id = a.user_id
-			   left join developers c on c.user_id = a.user_id
-			   left join role_users d on d.user_id = a.user_id
-			   left join roles e on e.id = d.role_id
-			   left join collaterals f on f.developer_id = c.id
-			   left join eforms g on g.user_id = a.user_id
-			   left join user_services h on h.pn = a.user_id
-			   left join properties i on i.developer_id = c.id");
+               , case when e.slug is not null then e.slug else h.role end as role
+               , i.name as project_name
+               , k.company_name
+               , case when c.id = '1' then 'Non Kerja Sama' when c.id >1 then 'Kerja Sama' else '' end as developer
+               , a.old_values
+               , a.new_values
+               , a.ip_address
+               , a.extra_params as action_location
+               from audits a
+               left join users b on b.id = a.user_id
+               left join developers c on c.user_id = a.user_id
+               left join user_developers j on j.user_id = b.id
+               left join developers k on k.user_id = j.admin_developer_id
+               left join role_users d on d.user_id = a.user_id
+               left join roles e on e.id = d.role_id
+               left join collaterals f on f.developer_id = c.id
+               left join eforms g on g.user_id = a.user_id
+               left join user_services h on h.pn = a.user_id
+               left join properties i on i.developer_id = c.id");
 
         //this for table view auditrail collateral
         \DB::unprepared("DROP VIEW IF EXISTS auditrail_collaterals");
