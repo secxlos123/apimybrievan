790733cc9b412e847eec1b33afaae21748876ebb
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-02-19 19:19:48 +0700

    fixrecontest mobile

--- app/Http/Controllers/API/v1/Int/RecontestController.php
+++ app/Http/Controllers/API/v1/Int/RecontestController.php
@@ -49,7 +49,7 @@ class RecontestController extends Controller
             $user_login = \RestwsHc::getUser();
 
             $eform = EForm::find($eform_id);
-        
+
             $typeModule = getTypeModule(EForm::class);
             notificationIsRead($eform_id, $typeModule);
 
@@ -59,7 +59,9 @@ class RecontestController extends Controller
             $recontest = $eform->recontest;
             $recontest->update($data);
 
-            $recontest->generateArrayData( $request->mutations, 'mutations' );
+            if ( $request->mutations ) {
+                $recontest->generateArrayData( $request->mutations, 'mutations' );
+            }
             $recontest->generateArrayData( $request->recontest, 'documents' );
 
             $credentials = [
