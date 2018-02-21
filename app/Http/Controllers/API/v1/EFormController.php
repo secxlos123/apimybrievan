d4845e38f19eb2e8f4d4c19b0a2afdbebcadcde9
secxlos <evan.tohap@lawencom.com>
2018-02-21 20:38:19 +0700

    update

--- app/Http/Controllers/API/v1/EFormController.php
+++ app/Http/Controllers/API/v1/EFormController.php
@@ -526,7 +526,7 @@ class EFormController extends Controller
 				
 				$customer = $customer->toArray();
 				$customer = json_decode(json_encode($customer), True);
-				$message = ['no_hp'=>$customer[0]['phone'],'no_reff'=>$kpr->ref_number,'nama_cust'=>$customer[0]['first_name'].' '.$customer[0]['last_name'],'kode_message'=>'1'];				
+				$message = ['no_hp'=>$customer[0]['mobile_phone'],'no_reff'=>$kpr->ref_number,'nama_cust'=>$customer[0]['first_name'].' '.$customer[0]['last_name'],'kode_message'=>'1'];				
 				\Log::info("-------------------sms notifikasi-----------------");
 				\Log::info($message);
 				$testing = app('App\Http\Controllers\API\v1\SentSMSNotifController')->sentsms($message);
