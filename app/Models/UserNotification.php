b6a75943dba3f1ebc829e9060fb80e731eb70230
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-02-26 11:13:59 +0700

    fix ao dispo

--- app/Models/UserNotification.php
+++ app/Models/UserNotification.php
@@ -50,13 +50,16 @@ class UserNotification extends Model
 	public function scopeAo( $query, $pn )
 	{
 		return $query->leftJoin( 'eforms', 'notifications.slug', '=', 'eforms.id' )
-			// ->leftJoin( 'collaterals', function ( $join ) {
-   //              $join->on( 'collaterals.id', '=' , 'notifications.slug' )
-   //                  ->where( 'notifications.type_module', '=', 'collateral' )
-   //                  ->where( 'notifications.type', '=', 'App\Notifications\CollateraAODisposition' );
-   //          })
-			->where( 'eforms.ao_id', $pn )
-			// ->orWhereNotNull( 'collaterals.id' )
+			->leftJoin( 'collaterals', function ( $join ) {
+                $join->on( 'collaterals.id', '=' , 'notifications.slug' )
+                    ->where( 'notifications.type_module', '=', 'collateral' )
+                    ->where( 'notifications.type', '=', 'App\Notifications\CollateraAODisposition' );
+            } )
+            ->where( function( $q ) use ( $pn ) {
+				$q->where( 'eforms.ao_id', $pn )
+				    ->orWhereNotNull( 'collaterals.id' )
+
+            } )
 			->whereIn(
 				'notifications.type'
 				, array(
