434efabc2aefd84990fe98dae16530ddec5624a8
Permana Motionald <motionalead@gmail.com>
2018-02-26 16:25:28 +0700

    drop unique

--- database/migrations/2018_02_26_162235_drop_index_crm_new_customers.php
+++ database/migrations/2018_02_26_162235_drop_index_crm_new_customers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditNullableFieldOnCrmNewCustomers extends Migration
+class DropIndexCrmNewCustomers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,8 @@ class EditNullableFieldOnCrmNewCustomers extends Migration
     public function up()
     {
         Schema::table('crm_new_customers', function (Blueprint $table) {
-		$table->string('nik')->nullable()->change();
-		$table->string('email')->nullable()->change();
+        	$table->dropUnique('nik');
+		$table->dropUnique('email');
         });
     }
 
@@ -27,8 +27,8 @@ class EditNullableFieldOnCrmNewCustomers extends Migration
     public function down()
     {
         Schema::table('crm_new_customers', function (Blueprint $table) {
-            $table->string('nik')->unique()->change();
-          $table->string('email')->unique()->change();
+		$table->unique('nik');
+		$table->unique('email');
         });
     }
 }
