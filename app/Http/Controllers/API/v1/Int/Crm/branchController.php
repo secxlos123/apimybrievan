21bbe237225174168f7a84f42503c97cb50196f5
Permana Motionald <motionalead@gmail.com>
2018-02-27 16:00:05 +0700

    list kanwil kanca uker

--- app/Http/Controllers/API/v1/Int/Crm/branchController.php
+++ app/Http/Controllers/API/v1/Int/Crm/branchController.php
@@ -11,16 +11,89 @@ class branchController extends Controller
 {
 	public function list_kanwil(Request $request)
 	{
-		$list_kanwil = RestwsHc::setBody([
-            		'request' => json_encode([
-                	'requestMethod' => 'get_list_kanwil',
-                	'requestData' => [
-                   		'app_id' => 'mybriapi'
-                		],
-            		])
-        	])
-		->post( 'form_params' );
+    	$list_kanwil = RestwsHc::setBody([
+            'request' => json_encode([
+                'requestMethod' => 'get_list_kanwil',
+                'requestData' => [
+                   'app_id' => 'mybriapi'
+                ],
+            ])
+        ])
+        ->post( 'form_params' );
 
-		return $list_kanwil;
-	}	
+      if ($list_kanwil['responseCode'] == '00' ) {
+
+         $list_kanwil[ 'responseData' ] = array_map( function( $content ) {
+          return [
+              'region_id' => $content[ 'region' ],
+              'region_name' => $content[ 'rgdesc' ],
+              'branch_id' => $content[ 'branch' ]
+          ];
+      }, $list_kanwil[ 'responseData' ] );
+        $kanwil_list = [
+            "current_page" => 1,
+            "data" => $list_kanwil[ 'responseData' ],
+            "from" => 1,
+            "last_page" => 1,
+            "next_page_url" => null,
+            "path" => "",
+            "per_page" => count( $list_kanwil[ 'responseData' ] ),
+            "prev_page_url" => null,
+            "to" => 1,
+            "total" => 1
+        ];
+        return response()->success( [
+            'message' => 'Sukses',
+            'contents' => $kanwil_list
+        ], 200 );
+
+      }
+
+     	return response()->error( [
+          'message' => $list_kanwil[ 'responseDesc' ]
+      	], 422 );
+
+	}
+
+	public function get_kanca_kanwil(Request $request)
+	{
+			$requestPost =[
+				'app_id' => 'mybriapi',
+				'region' => $request['region']
+			];
+
+			$list_kanca_kanwil = RestwsHc::setBody([
+						'request' => json_encode([
+								'requestMethod' => 'get_list_kanca_from_kanwil',
+								'requestData' => $requestPost
+						])
+				])
+				->post( 'form_params' );
+
+			return response()->success( [
+					'message' => 'Sukses',
+					'contents' => $list_kanca_kanwil
+			], 200 );
+	}
+
+	public function get_uker_kanca(Request $request)
+	{
+			$requestPost =[
+				'id_user' => $request->header('pn'),
+				'kode_branch' => $request['branch']
+			];
+
+			$list_uker_kanca = RestwsHc::setBody([
+						'request' => json_encode([
+								'requestMethod' => 'get_list_uker',
+								'requestData' => $requestPost
+						])
+				])
+				->post( 'form_params' );
+
+			return response()->success( [
+					'message' => 'Sukses',
+					'contents' => $list_uker_kanca
+			], 200 );
+	}
 }
