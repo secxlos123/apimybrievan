d3918791eb176a07d71523e1a355d22a1d325f6b
Erwan Akse <erwanakse@gmail.com>
2018-02-27 15:10:32 +0700

    fix redundant code on middleware

--- app/Http/Middleware/IpWhiteList.php
+++ app/Http/Middleware/IpWhiteList.php
@@ -13,7 +13,7 @@ class IpWhiteList {
 	 * @return mixed
 	 */
 	public function handle($request, Closure $next) {
-        $ip = $ip = env('ACCESS_CLAS_IP','10.35.65.167');
+        $ip = env('ACCESS_CLAS_IP','10.35.65.167');
 		if ($request->ip() != $ip) {
 			return response()->json(['error' => 401, 'message' => 'Unauthorized action.'], 401);
 		}
