8e9ea39072fd114a185dc13af123861947c43380
Permana Motionald <motionalead@gmail.com>
2018-03-06 16:47:34 +0700

    fix report marketing

--- app/Http/Controllers/API/v1/Int/Crm/reportController.php
+++ app/Http/Controllers/API/v1/Int/Crm/reportController.php
@@ -15,11 +15,12 @@ class reportController extends Controller
 {
     public function report_marketings(Request $request)
     {
-      $region = $request->input('region');
       if($request->has('region')){
+        $region = $request->input('region');
         $list_kanca = $this->list_kanca_for_kanwil($region);
       }else{
         $list_kanca = $this->list_all_kanca();
+        $region = $list_kanca[$request->header('branch')]['region_id'];
       }
       $list_kanwil = array_column($this->list_kanwil(),'region_name', 'region_id');
       $data = Marketing::getReports($request)->get();
@@ -157,7 +158,12 @@ class reportController extends Controller
       foreach ($list_kanwil as $key => $value) {
         $list_kanca = $this->get_kanca_kanwil($value['region_id']);
         foreach ($list_kanca['responseData'] as $key_kanca => $value_kanca) {
-         $list_all_kanca[$con[$len-strlen($value_kanca['mainbr'])].$value_kanca['mainbr']] = $value_kanca['mbdesc']
+         $list_all_kanca[$con[$len-strlen($value_kanca['mainbr'])].$value_kanca['mainbr']] =
+         [
+           'mbdesc' => $value_kanca['mbdesc'],
+           'region_id' => $value['region_id']
+
+           ]
           ;
         }
       }
