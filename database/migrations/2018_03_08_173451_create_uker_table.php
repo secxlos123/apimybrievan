8cd78c3e1be2394a4650bf2be8f93d477978ba8e
secxlos <evan.tohap@lawencom.com>
2018-03-08 17:52:14 +0700

    tambah

---
+++ database/migrations/2018_03_08_173451_create_uker_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUkerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+          Schema::create('uker_tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kanwil');
+            $table->string('id_uker');
+            $table->string('unit_kerja');
+            $table->string('unit_induk');
+            $table->string('kanca_induk');
+            $table->string('jenis_uker');
+            $table->string('kode_uker');
+            $table->string('dati2');
+            $table->string('dati1');
+            $table->string('alamat');
+            $table->string('no_telp');
+            $table->string('no_fax');
+            $table->string('koordinat');
+            $table->string('latitude');
+            $table->string('longitude');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kartu_kredit_details');
+    }
+}
