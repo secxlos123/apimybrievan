0df1cfbfb78e7ea2e4ffba73e912e77a82f8a804
Permana Motionald <motionalead@gmail.com>
2018-03-14 17:50:14 +0700

    Marketing request Delete Model

---
+++ app/Models/Crm/MarketingDeleteRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\Crm;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class MarketingDeleteRequest extends Model
+{
+    protected $table = 'marketing_delete_requests';
+
+    protected $fillable = ['pn','branch','marketing_id','status'];
+
+    public function scopeGetRequestDelete($query, Request $request)
+    {
+      return $query
+            ->orderBy('marketing_delete_requests.created_at', 'asc')
+            ->where( function($deleteRequest) use($request){
+              $deleteRequest->where( 'marketing_delete_requests.branch', '=', $request->header( 'branch' ) );
+              $deleteRequest->where( 'marketing_delete_requests.status', '=', 'req' );
+            })
+            ;
+    }
+}
