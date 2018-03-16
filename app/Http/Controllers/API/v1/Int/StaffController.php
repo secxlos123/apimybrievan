cb6dd127303f26cff5db2e429f8b05594d7ec1cb
rachmat ramadhan <rachmat.ramadhan@wgs.co.id>
2018-03-16 14:29:42 +0700

    change code

--- app/Http/Controllers/API/v1/Int/StaffController.php
+++ app/Http/Controllers/API/v1/Int/StaffController.php
@@ -15,29 +15,29 @@ class StaffController extends Controller
             return response()->success(
                 [
                     'message' => 'Sukses',
-                    'contents' => {
+                    'contents' => [
                         "current_page": 1,
                         "data": [
-                            {
+                            [
                                 "id": "00137746",
                                 "name": "Achmad Irfan Ramadhan"
-                            },
-                            {
+                            ],
+                            [
                                 "id": "00137746",
                                 "name": "Dedi Idrus"
-                            },
-                            {
+                            ],
+                            [
                                 "id": "00137746",
                                 "name": "Rosiva Corry Gustina Sitorus"
-                            },
-                            {
+                            ],
+                            [
                                 "id": "00137746",
                                 "name": "Ryanto Imam Wibowo"
-                            },
-                            {
+                            ],
+                            [
                                 "id": "00137746",
                                 "name": "Gustiawan Aswinardian"
-                            }
+                            ]
                         ],
                         "from": 1,
                         "last_page": 1,
@@ -47,7 +47,7 @@ class StaffController extends Controller
                         "prev_page_url": null,
                         "to": 1,
                         "total": 1
-                    }
+                    ]
                 ]
                 , 200
             );
