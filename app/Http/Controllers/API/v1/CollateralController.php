51768c3605f3f553945789132a37e3f1068e843f
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-04-04 14:42:25 +0700

    fix search on datatable collateral

--- app/Http/Controllers/API/v1/CollateralController.php
+++ app/Http/Controllers/API/v1/CollateralController.php
@@ -90,7 +90,7 @@ class CollateralController extends Controller
       if ($this->request->has('search'))
         $data->whereHas('property',function($property) use ($request)
         {
-          $property->where('name','ilike','%'.$request->input('search').'%');
+          $property->where(\DB::raw('LOWER(name)'),'ilike','%'.$request->input('search').'%');
         });
       if ($this->request->has('slug')) {
           $data->where('id',$this->request->input('slug'));
@@ -117,6 +117,12 @@ class CollateralController extends Controller
       else
       {
           $data->where('region_id',$region['region_id']);
+      }
+      if ($this->request->has('search')){
+        $data->whereHas('property',function($property) use ($request)
+        {
+          $property->where(\DB::raw('LOWER(name)'),'ilike','%'.$request->input('search').'%');
+        });
       }
       if ($this->request->has('slug')) {
           $data->where('id',$this->request->input('slug'));
