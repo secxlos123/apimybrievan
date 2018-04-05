ef049e84264e446ab60533c58e0c5384af2c00fa
secxlos <evan.tohap@lawencom.com>
2018-04-05 15:28:26 +0700

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -109,7 +109,7 @@ class Customer extends User
      */
     public function getPersonalAttribute()
     {
-		$IsFinish = DB::table('customer_detail')->select('customer_detail.IsFinish')->where('user_id',$this->detail->user_id)->max('id')->get();
+		$IsFinish = DB::table('customer_detail')->where('user_id',$this->detail->user_id)->max('IsFinish')->get();
         $personal_data = [
             'user_id' => $this->detail ? $this->detail->user_id : '',
             'name' => $this->fullname,
