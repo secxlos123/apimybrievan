818d0a137eee542b3bb8011d71ddf79e3c183493
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-04-13 17:22:45 +0700

    log appointment

--- app/Http/Controllers/API/v1/AppointmentController.php
+++ app/Http/Controllers/API/v1/AppointmentController.php
@@ -112,6 +112,8 @@ class AppointmentController extends Controller
     public function store(CreateRequest $request)
     {
         $postTaken = ['title', 'appointment_date', 'user_id', 'ao_id', 'eform_id', 'ref_number', 'address', 'latitude', 'longitude', 'guest_name', 'desc', 'status'];
+        \Log::info("===========================POST Schedule=======================");
+        \Log::info($request->only($postTaken));
         $save = Appointment::create($request->only($postTaken));
 
         if ($save) {
