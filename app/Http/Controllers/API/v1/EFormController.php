d43dc2cbccfc2a1d6ebf912ee312853dbe850724
Aswin <csbcan57@gmail.com>
2018-04-24 11:42:39 +0700

    fix bugs

--- app/Http/Controllers/API/v1/EFormController.php
+++ app/Http/Controllers/API/v1/EFormController.php
@@ -265,9 +265,14 @@ class EFormController extends Controller
           $eform[0]['customer']['schedule'] = [];
           $eform[0]['customer']['is_approved'] = $eform[0]['is_approved'];
 
-          // $eform[0]['Url'] = env('APP_URL').'/uploads/';
           // keperluan tot
-          $eform[0]['Url'] = 'http://103.63.96.167/api/uploads/';
+          $host = env('APP_URL');
+          if($host == 'http://103.63.96.167/api/'){     
+              $eform[0]['Url'] = 'http://103.63.96.167/api/uploads/';
+          }else{
+              $eform[0]['Url'] = env('APP_URL').'/uploads/';
+          }
+          
 
           $eform[0]['nominal'] = $eform[0]['request_amount'];
           $eform[0]['costumer_name'] = $customer[0]['first_name'].' '.$customer[0]['last_name'];
