9a75287f0d18e294dbf662c8a0349632aa5e2754
rangga.darmajati <rangga.darmajati@wgs.co.id>
2018-04-24 11:55:51 +0700

    Change AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,25 +12,32 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
-        if ( ENV("APP_ENV") == "local" && ENV('IS_SSL') == 0 ) {
-            \DB::listen(function ($query) {
-                \Log::info("--- start query ---");
-                \Log::info("query : " . $query->sql);
-                \Log::info("bindings : " . json_encode($query->bindings));
-                \Log::info("time : " . $query->time);
-                \Log::info("--- end query ---");
-            });
-        } else if ( ENV("APP_ENV") == "production" && ENV('IS_SSL') == 0 ) {
-            \DB::listen(function ($query) {
-                \Log::info("--- start query ---");
-                \Log::info("query : " . $query->sql);
-                \Log::info("bindings : " . json_encode($query->bindings));
-                \Log::info("time : " . $query->time);
-                \Log::info("--- end query ---");
-            });
-        } else {
+        // if ( ENV("APP_ENV") == "local" && ENV('IS_SSL') == 0 ) {
+        //     \DB::listen(function ($query) {
+        //         \Log::info("--- start query ---");
+        //         \Log::info("query : " . $query->sql);
+        //         \Log::info("bindings : " . json_encode($query->bindings));
+        //         \Log::info("time : " . $query->time);
+        //         \Log::info("--- end query ---");
+        //     });
+        // } else if ( ENV("APP_ENV") == "production" && ENV('IS_SSL') == 0 ) {
+        //     \DB::listen(function ($query) {
+        //         \Log::info("--- start query ---");
+        //         \Log::info("query : " . $query->sql);
+        //         \Log::info("bindings : " . json_encode($query->bindings));
+        //         \Log::info("time : " . $query->time);
+        //         \Log::info("--- end query ---");
+        //     });
+        // } else {
+        //     \URL::forceScheme('https');
+        // }
+        $host = $request->getSchemeAndHttpHost();
+        $cek = substr($host, 0,8);
+        \Log::info($cek);
+        if( $cek == 'https://' ){
+            \Log::info("HTTPS");
             \URL::forceScheme('https');
         }
     }
