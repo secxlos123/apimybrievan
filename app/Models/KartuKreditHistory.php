08366179891e1a34952ba00623a50e2f0fddc207
mohammad kukuh <emkukuh@gmail.com>
2018-05-14 14:06:18 +0700

    kredit history

--- app/Models/KartuKreditHistory.php
+++ app/Models/KartuKreditHistory.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class KartuKreditHistory extends Model
 {
     protected $table = 'kartu_kredit_history';
+
+    protected $fillable = ['apregno','kodeproses','kanwil','pn'];
+
+    protected $appends = ['deskripsi_proses'];
+
+    public function getDescriptionProsesAttribute($val){
+    	if($val == '1'){
+    		return 'Ajukan';
+    	}else if($val == '3.1'){
+    		return 'Verifikasi';
+    	}else if($val == '6.1'){
+    		return 'Analis';
+    	}else if($val == '7.1'){
+    		return 'Putusan Approve';
+    	}else if($val == '8.1'){
+    		return 'Putusan Reject';
+    	}
+    }
 }
