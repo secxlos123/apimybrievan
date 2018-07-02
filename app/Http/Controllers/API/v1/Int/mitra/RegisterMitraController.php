f4f804c6308959ea75249b0cb3a8d883560639b0
secxlos <evan.tohap@lawencom.com>
2018-07-02 11:18:32 +0700

    update mitra

--- app/Http/Controllers/API/v1/Int/mitra/RegisterMitraController.php
+++ app/Http/Controllers/API/v1/Int/mitra/RegisterMitraController.php
@@ -98,6 +98,11 @@ class RegisterMitraController extends Controller
 		}elseif($baseRequest['lo_mitra']=='mitra_pemutus'){
 			$mitra = mitra5::create( $baseRequest );
 		}elseif($baseRequest['lo_mitra']=='mitra_las'){
+			  $golongan = DB::table('jenis_mitra_kerjasama')
+                ->select('jenis_mitra_kerjasama."KODE_LAS"')
+                ->where('jenis_mitra_kerjasama."KODE"', $baseRequest['jenis_instansi'])
+                ->get();
+                $baseRequest['jenis_instansi'] = $golongan[0]->KODE.' '.$golongan[0]->KODE;
 			$client = $this->client();
             $mitra = $client->insertUpdateInstansiBRI($baseRequest);
 		}
