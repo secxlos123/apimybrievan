e8155f574126855ed95de9f089c18d4ebb529ac8
Auzan <auzanlazuardi@gmail.com>
2018-07-04 14:59:19 +0700

    migration ulang name,pn pinca

--- database/migrations/2018_07_04_145727_add_namepnpinca_to_field_kkh.php
+++ database/migrations/2018_07_04_145727_add_namepnpinca_to_field_kkh.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNamepnpincaToFieldKkhtable extends Migration
+class AddNamepnpincaToFieldKkh extends Migration
 {
     /**
      * Run the migrations.
